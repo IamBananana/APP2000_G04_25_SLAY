157fda0aa1bcb03786009af1f6f43a158bddd366
Ulrikke Ekedal <ulli.ekedal@hotmail.com>
2025-01-06 13:45:22 +0100

    Løst konflikter, optimalisert avhengigheter og ryddet opp i App.tsx og index.css

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,19 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/Navbar';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './register/register';
-
-const App: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
-
-=======
 
 const App: React.FC = () => {
   // State for å holde styr på telleren
   const [count, setCount] = useState<number>(0);
 
   // Funksjon for å håndtere knappetrykk
->>>>>>> main
   const incrementCounter = () => {
     setCount(count + 1);
   };
 
-<<<<<<< HEAD
-=======
   // Definerer lenker som skal være i navbaren på de forskjellige sidene
->>>>>>> main
   const navLinks = [
     { name: "Hjem", href: "/" },
     { name: "Om oss", href: "/omOss" },
@@ -39,7 +29,6 @@ const App: React.FC = () => {
     { name: "Mitt lag", href: "/lag" },
     { name: "Min side", href: "/minSide" },
     { name: "Login", href: "/login" },
-<<<<<<< HEAD
     { name: "Registrer", href: "/register" },
   ];
 
@@ -61,22 +50,6 @@ const App: React.FC = () => {
         </Routes>
       </div>
     </Router>
-=======
-    { name: "Registrer", href: "/registrer" },
-  ];
-
-  return (
-    <div>
-      <AppNavbar links={navLinks} />
-      <div className="container" style={{ paddingTop: '70px' }}>
-        <h1 className="text-center">Velkommen til React med Bootstrap!</h1>
-        <h2 className="text-center">Teller: {count}</h2>
-        <button className="btn btn-primary" onClick={incrementCounter}>
-          Øk teller
-        </button>
-      </div>
-    </div>
->>>>>>> main
   );
 };
 
