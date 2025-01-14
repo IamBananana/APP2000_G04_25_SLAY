f1d6adf38e9cabb1f72b01b82b0ecd4c351aa8c3
sUmi7ro <simonto.no@outlook.com>
2025-01-14 01:54:04 +0100

    HOOOooOOOooOOooooOoooOOookss

--- src/spill/page.tsx
+++ src/spill/page.tsx
@@ -2,35 +2,42 @@ import { useState } from "react";
 
 export default function Spill(){
     const maxSpillere : number = 6;
+    const [inpPlayerCount, setInpPlayerCount] = useState(0);
+    let addPlayers = () => {
+        console.log("Button clicked!");
+    };
+
+    const items = <div className="mx-4">
+                        <div className="fs-6 mb-1">Ant Spillere: (max {maxSpillere})</div>
+                        <input id="inpCountPlayers" type="number" value={inpPlayerCount} onChange={
+                            (e) => {
+                                const value = parseInt(e.target.value) || 0;
+                                setInpPlayerCount(value); 
+                                console.log(value);
+                            }}  
+                                className="form-control me-auto mb-2 w-75" required min="1" max={maxSpillere}/>
+                        <button type="button" className="btn btn-outline-primary w-50 text-center me-auto"
+                        onClick={addPlayers}>Legg til</button>
+                    </div>;
+
+    const [menuItems, setMenuItems] = useState(items);
 
     const addPlayer = () => {
         //Legger til bare en spiller
     }
 
-    const addPlayers = () => {
+    addPlayers = () => {
         //Legger til antall spillere brukeren har oppgitt
-
-        const [menuItems, setMenuItems] = useState(0);
-
-        const menu = document.getElementById("menu");
-        let inpCountPlayers: any = document.getElementById("inpCountPlayers");
-        
-
-        //omg fuck this
-        console.log(menu != null && inpCountPlayers != null);
-        if(menu != null && inpCountPlayers != null) {
-            menu.innerHTML = '';
-            menu.innerHTML += '<i className="bi bi-plus ms-auto me-1 fs-5" style={{caretColor: "transparent", cursor: "pointer", width: "100%"}} onClick={addPlayer}></i>';
-            
-            for(let i: any = 0; i<inpCountPlayers; i++){
-                menu.innerHTML += '<div className="mt-4 mx-3">' +
-                                        '<div className="input-group">' +
-                                            '<label htmlFor="inpSpiller" className="input-group-text" style={{fontSize: "80%"}}>Spiller 1:</label>' +
-                                            '<input id="inpNavn1" type="text" className="form-control" />' +
-                                        '</div>' +
-                                    '</div>';
-            }
+        {/*
+        for(let i = 0; i<inpPlayerCount; i++){
+            setMenuItems(<div className="mt-4 mx-3">
+                <div className="input-group">
+                    <label htmlFor="inpSpiller" className="input-group-text" style={{fontSize: "80%"}}>Spiller 1:</label>
+                    <input id="inpNavn1" type="text" className="form-control" />
+                </div>
+            </div>)
         }
+        */}
     }
 
     return (
@@ -40,12 +47,8 @@ export default function Spill(){
                 <div id="map" className="container border border-secondary col-9 p-5 me-1">Kart placeholder</div>
                 <div id="options" className="d-flex flex-column align-items-center mx-0 border border-secondary rounded col-2 text-nowrap">
                     <div id="menu" className="mt-2 d-flex flex-column align-items-column">
-                        <div className="mx-4">
-                            <div className="fs-6 mb-1">Ant Spillere: (max {maxSpillere})</div>
-                            <input id="inpCountPlayers" className="form-control me-auto mb-2 w-75" required pattern="[1-6] {1}"/>
-                            <button type="button" className="btn btn-outline-primary w-50 text-center me-auto"
-                            onClick={addPlayers}>Legg til</button>
-                        </div>
+
+                        {menuItems}
                         
                         {/* 
                             Pluss tegn 
