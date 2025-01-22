46c866533ba9f2276fe80c85c2f550def6ab22d9
IamBananana <richardgjelland@hotmail.com>
2025-01-22 11:17:59 +0100

    ye

--- src/App.tsx
+++ src/App.tsx
@@ -34,11 +34,11 @@ const App: React.FC = () => {
       <AppNavbar links={navLinks} />
       <div className="main-container">
         <h1>TeeTime</h1>
-        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+        <div id="carouselWoo" className="carousel slide" data-bs-ride="carousel">
           <div className="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselWoo" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselWoo" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselWoo" data-bs-slide-to="2" aria-label="Slide 3"></button>
           </div>
           <div className="carousel-inner">
             <div className="carousel-item active">
@@ -51,11 +51,11 @@ const App: React.FC = () => {
               <img src={stockGolf3} className="d-block carousel-image" alt="Slide 3" />
             </div>
           </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselWoo" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Previous</span>
           </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselWoo" data-bs-slide="next">
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Next</span>
           </button>
@@ -65,7 +65,8 @@ const App: React.FC = () => {
           <h2>Velkommen til TeeTime
           </h2>
             <p>
-            TeeTime er en nettside for deg som liker golf. Her kan du finne informasjon om golfbaner, <br></br>bestille starttider, og delta i turneringer. Vi tilbyr også en rekke artikler og tips for å 
+            TeeTime er en nettside for deg som liker golf. Her kan du finne informasjon om golfbaner, <br>
+            </br>bestille starttider, og delta i turneringer. Vi tilbyr også en rekke artikler og tips for å 
             forbedre ditt spill. Bli med i vårt fellesskap og del din lidenskap for golf med andre entusiaster.
             </p>
         </div>
