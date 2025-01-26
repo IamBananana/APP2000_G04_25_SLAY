14720574c0adeda53dbf69012f5c641018d3efe9
Ulrikke Ekedal <ulli.ekedal@hotmail.com>
2025-01-26 14:08:53 +0100

    Ny register look

--- src/register/register.tsx
+++ src/register/register.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import './Register.css';
-import '../index.css';
+import logo from '../assets/android-chrome-512x512.png';
 
 const Register: React.FC = () => {
   const [username, setUsername] = useState('');
@@ -12,29 +11,66 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className="register-container">
-      <form className="register-form">
-        <h1>Registrer ny bruker</h1>
-        <input 
-          type="text" 
-          placeholder="Brukernavn" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} 
+    <div
+      id="wrapper"
+      className="container col-md-5 d-flex flex-column justify-content-around align-items-center border border-success
+                 bg-light rounded p-5 mt-5"
+    >
+      <img src={logo} className="img w-25" alt="logo" />
+
+      <div className="container text-center" style={{ color: '#000' }}>
+        <p className="display-6 mb-0 fs-2 fw-normal">Registrer deg</p>
+        <p className="display-12">It's TeeTime</p>
+      </div>
+
+      <div className="form-floating mb-3">
+        <input
+          id="inpBrukernavn"
+          className="form-control rounded-pill"
+          type="text"
+          placeholder="Brukernavn"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
-        <input 
-          type="email" 
-          placeholder="E-post" 
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)} 
+        <label htmlFor="inpBrukernavn" style={{ padding: 'calc(var(bs-gutter-x)* .5)' }}>
+          Brukernavn
+        </label>
+      </div>
+
+      <div className="form-floating mb-3">
+        <input
+          id="inpEmail"
+          className="form-control rounded-pill"
+          type="email"
+          placeholder="E-post"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <input 
-          type="password" 
-          placeholder="Passord" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
+        <label htmlFor="inpEmail">E-post</label>
+      </div>
+
+      <div className="form-floating mb-1">
+        <input
+          id="inpPassord"
+          className="form-control rounded-pill"
+          type="password"
+          placeholder="Passord"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button onClick={handleRegister}>Opprett Bruker</button>
-      </form>
+        <label htmlFor="inpPassord">Passord</label>
+      </div>
+
+      <button
+        className="btn btn-success btn-lg rounded-pill mt-3 mb-2 col-lg-3"
+        onClick={handleRegister}
+      >
+        Registrer
+      </button>
+
+      <div className="mb-4">
+        Har du allerede bruker? <a href="/" className="text-decoration-none">Logg inn</a>
+      </div>
     </div>
   );
 };
