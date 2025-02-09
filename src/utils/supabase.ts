21e22282abb6213bc6ca3c59b27f5929c6d4321c
Ulrikke Ekedal <ulli.ekedal@hotmail.com>
2025-02-09 15:49:01 +0100

    fiksa next_public_

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client
 const supabase = createClient(
-    process.env.SUPABASE_URL as string,
-    process.env.SUPABASE_ANON_KEY as string
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
 export default supabase;
